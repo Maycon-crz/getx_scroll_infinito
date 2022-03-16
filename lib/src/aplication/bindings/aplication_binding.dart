10ab7f2d48b95050559e9d0428d30b077c15ef35
FlutLab.io <>
2022-03-16 04:41:07 +0000

    Autosave

--- lib/src/aplication/bindings/aplication_binding.dart
+++ lib/src/aplication/bindings/aplication_binding.dart
@@ -4,6 +4,6 @@ import 'package:dio/dio.dart';
 class ApplicationBinding implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut(() => Dio(BaseOption(baseUrl: 'https://5f7cba02834b5c0016b058aa.mockapi.io/api')));
+    Get.lazyPut(() => Dio(BaseOptions(baseUrl: 'https://5f7cba02834b5c0016b058aa.mockapi.io/api')));
   }
 }
